951156957eec789940d7873b42125bbb4e98d0f6
Virag Hote <viraghote2020@gmail.com>
2025-11-09 17:23:41 +0530

    Fixed vite.config.js merge conflict

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-=======
-import tailwindcss from '@tailwindcss/vite'
-
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
->>>>>>> master
-})
+  plugins: [react(), tailwindcss()],
+});
